02336d9ff0739a8623257c3b64d71a072051d550
Sophia Westwood <sophia@cs.stanford.edu>
2020-12-29 00:05:34 -0800

    Day 14a

--- fourteen.go
+++ fourteen.go
@@ -26,7 +26,7 @@ func loadProgram() []command {
 	commands := make([]command, 0)
 	for scanner.Scan() {
 		// Example: mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-		// or mem[8] = 11
+		//          mem[8] = 11
 		line := scanner.Text()
 		if err != nil {
 			log.Fatal(err)
@@ -35,9 +35,8 @@ func loadProgram() []command {
 		if parts[0] == "mask" {
 			commands = append(commands, command{mask: parts[1]})
 		} else {
-			indexStr := parts[0][3:]
-			// indexStr = indexStr[:len(indexStr)-1]
-			index, _ := strconv.Atoi(indexStr)
+			indexStr := parts[0][4:]
+			index, _ := strconv.Atoi(indexStr[:len(indexStr)-1])
 			value, _ := strconv.Atoi(parts[1])
 			commands = append(commands, command{index: index, value: value})
 		}
@@ -50,5 +49,30 @@ func loadProgram() []command {
 
 func main() {
 	commands := loadProgram()
-	fmt.Println(commands)
+	memory := make(map[int]int)
+	mask := ""
+	for _, command := range commands {
+		if len(command.mask) > 0 {
+			mask = command.mask
+		} else {
+			value := command.value
+			for i, maskRune := range mask {
+				if maskRune != 'X' {
+					maskBit, _ := strconv.Atoi(string(maskRune))
+					if maskBit == 0 {
+						// Clear this bit
+						value &= ^(1 << (35 - i))
+					} else {
+						value |= (1 << (35 - i))
+					}
+				}
+			}
+			memory[command.index] = value
+		}
+	}
+	sum := 0
+	for _, contents := range memory {
+		sum += contents
+	}
+	fmt.Println("sum of memory is", sum)
 }
